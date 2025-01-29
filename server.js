e38d651824c81a16a9a5c4c8f9bff8848a400455
Your Name <your.email@example.com>
2025-01-30 00:19:38 +0530

    lil

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const server = http.createServer(app);
 
 
 app.use(cors({
-  origin: ["http://localhost:3000", "https://joyful-froyo-69e8c4.netlify.app/" ],
+  origin: ["http://localhost:3000", "https://joyful-froyo-69e8c4.netlify.app" ],
 methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true, 
 }));
