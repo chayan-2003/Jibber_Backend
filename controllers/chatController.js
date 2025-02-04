2f1f095b9adf4bee2861cf849aad5a813da89d93
Your Name <your.email@example.com>
2025-02-05 03:22:56 +0530

    IMPLEMENTED_REDIS_FOR_FASTER_QUERY

--- controllers/chatController.js
+++ controllers/chatController.js
@@ -1,6 +1,9 @@
 import express from 'express';
 import Chat from '../models/Chat.js';
+import Redis from 'ioredis';
 
+const redisClient = new Redis();
+const DEFAULT_EXPIRATION = 3600;
 export const createChat = async (req, res) => {
     const { room, message } = req.body;
     try {
@@ -17,10 +20,30 @@ export const createChat = async (req, res) => {
 }
 //get message for a group 
 export const getChat = async (req, res) => {
-    try {
-        const chat = await Chat.find({ room: req.params.roomId }).populate('user', 'username').sort({ createdAt: 1 });
-        res.status(200).json(chat);
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
+    redisClient.get(`chat:${req.params.roomId}`, async (err, data) => {
+        if (err) {
+            console.error("Error fetching from Redis:", err);
+            res.status(500).json({ message: "Internal Server Error" });
+            return;
+        }
+        if(data){
+            res.status(200).json(JSON.parse(data));
+            console.log("cache hit");
+        }
+        else
+        {
+            console.log("cache miss");
+            try {
+                const chat = await Chat.find({ room: req.params.roomId }).populate('user', 'username').sort({ createdAt: 1 });
+                redisClient.setex(`chat:${req.params.roomId}`, DEFAULT_EXPIRATION, JSON.stringify(chat), (err) => {
+                    if (err) {
+                        console.error("Error setting Redis key:", err);
+                    }
+                });
+                res.status(200).json(chat);
+            } catch (error) {
+                res.status(500).json({ error: error.message });
+            }
+        }
+    });
 }
